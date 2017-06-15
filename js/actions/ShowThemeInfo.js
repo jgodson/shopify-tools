be817ca29a58c264cab459442b5c484b12c10c1b
Jason Godson <jason.godson@shopify.com>
2017-06-15 15:07:39 -0600

    fix theme info bug and change strucutre

--- js/actions/ShowThemeInfo.js
+++ js/actions/ShowThemeInfo.js
@@ -12,27 +12,55 @@ if (!document.querySelector('.shopify-tools-css')) {
 
 $('.shopify-tools-css')[0].sheet.insertRule(CSS, 0);
 
+// Regex to use to find theme id in url
+var THEME_ID_REGEX = new RegExp(/\/([0-9]*)\//);
+
 // Get theme information
 $.get(`${document.origin}/admin/themes.json`)
   .always((response) => {
     // In this case, we don't have a status if the request was successful
     if (response.status) {
-      console.error(`There was a problem getting theme info. Reponse came back with ${response.status} - ${response.statusText}`);
+      console.error(`There was a problem getting theme info.
+        Reponse came back with ${response.status} - ${response.statusText}`
+      );
       return;
     }
+
     var themeInfo = response.themes;
-    var themes = $('.published-theme-title, .unpublished-theme-title');
-    for (let index = 0; index < themes.length; index++) {
-      $(themes[index]).parent().append(generateThemeInfoHTML(themeInfo[index]));
+    var $themes = $('.published-theme, .unpublished-themes');
+    
+    for (let index = 0; index < $themes.length; index++) {
+      let $currentTheme = $($themes[index]);
+      currentId = null; 
+      if ($currentTheme.hasClass('published-theme')) {
+        let url = ($currentTheme.find('[href*="/editor"]').attr('href'));
+        currentId = THEME_ID_REGEX.exec(url)[1];
+      } else {
+        currentId = $currentTheme.attr('id').replace('theme_', "");
+      }
+      let currentInfo = matchThemeInfo(currentId, themeInfo);
+      let $target = $currentTheme.find('[class*="theme-title"]').parent();
+      $target.append(generateThemeInfoHTML(currentInfo));
     }
   });
 
 // Helper function
 function generateThemeInfoHTML(info) {
   return `
-  <div class="shopify-tools-theme-info">
-    <p>Theme ID: ${info.id}</p>
-    <p>Last updated ${moment(info.updated_at).fromNow()}</p>
-  </div>
+    <br/>
+    <div class="shopify-tools-theme-info">
+      <p>Theme ID: ${info.id}</p>
+      <p>Last updated ${moment(info.updated_at).fromNow()}</p>
+    </div>
   `;
+}
+
+function matchThemeInfo(themeId, themeInfo) {
+  for (let index = 0; index < themeInfo.length; index++) {
+    // Comparing a number to a string so using == instead of === here
+    if (themeInfo[index].id == themeId) {
+      return themeInfo[index];
+    }
+  }
+  return undefined;
 }
