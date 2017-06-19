07f0077b2584da3bb8cbeefb2f873faa042b8dea
Jason Godson <jason.godson@shopify.com>
2017-06-19 11:56:08 -0600

    [BugFix] make sure theme info doesn't show twice

--- js/actions/ShowThemeInfo.js
+++ js/actions/ShowThemeInfo.js
@@ -6,7 +6,7 @@ var CSS = `
     color: #81878d;
   }
 `
-if (!document.querySelector('.shopify-tools-css')) {
+if ($('.shopify-tools-css').length === 0) {
   $('head').append('<style class="shopify-tools-css">');
 }
 
@@ -15,34 +15,37 @@ $('.shopify-tools-css')[0].sheet.insertRule(CSS, 0);
 // Regex to use to find theme id in url
 var THEME_ID_REGEX = new RegExp(/\/([0-9]*)\//);
 
-// Get theme information
-$.get(`${document.origin}/admin/themes.json`)
-  .always((response) => {
-    // In this case, we don't have a status if the request was successful
-    if (response.status) {
-      console.error(`There was a problem getting theme info.
-        Reponse came back with ${response.status} - ${response.statusText}`
-      );
-      return;
-    }
-
-    var themeInfo = response.themes;
-    var $themes = $('.published-theme, .unpublished-themes');
-    
-    for (let index = 0; index < $themes.length; index++) {
-      let $currentTheme = $($themes[index]);
-      currentId = null; 
-      if ($currentTheme.hasClass('published-theme')) {
-        let url = ($currentTheme.find('[href*="/editor"]').attr('href'));
-        currentId = THEME_ID_REGEX.exec(url)[1];
-      } else {
-        currentId = $currentTheme.attr('id').replace('theme_', "");
+// Make sure theme info is not already displayed
+if ($('.shopify-tools-theme-info').length === 0) {
+  // Get theme information
+  $.get(`${document.origin}/admin/themes.json`)
+    .always((response) => {
+      // In this case, we don't have a status if the request was successful
+      if (response.status) {
+        console.error(`There was a problem getting theme info.
+          Reponse came back with ${response.status} - ${response.statusText}`
+        );
+        return;
       }
-      let currentInfo = matchThemeInfo(currentId, themeInfo);
-      let $target = $currentTheme.find('[class*="theme-title"]').parent();
-      $target.append(generateThemeInfoHTML(currentInfo));
-    }
-  });
+
+      var themeInfo = response.themes;
+      var $themes = $('.published-theme, .unpublished-themes');
+      
+      for (let index = 0; index < $themes.length; index++) {
+        let $currentTheme = $($themes[index]);
+        currentId = null; 
+        if ($currentTheme.hasClass('published-theme')) {
+          let url = ($currentTheme.find('[href*="/editor"]').attr('href'));
+          currentId = THEME_ID_REGEX.exec(url)[1];
+        } else {
+          currentId = $currentTheme.attr('id').replace('theme_', "");
+        }
+        let currentInfo = matchThemeInfo(currentId, themeInfo);
+        let $target = $currentTheme.find('[class*="theme-title"]').parent();
+        $target.append(generateThemeInfoHTML(currentInfo));
+      }
+    });
+}
 
 // Helper function
 function generateThemeInfoHTML(info) {
