3559f771e5585c405502777a35947ccf3a974f0b
Jason Godson <jgodson@users.noreply.github.com>
2017-06-22 11:02:03 -0600

    [BugFix + Refactor] Fix theme info duplicates and refactor (#6)
    * [BugFix] make sure theme info doesn't show twice
    
    * Fix theme info removed after duplcating theme. Refactor code

--- js/actions/ShowThemeInfo.js
+++ js/actions/ShowThemeInfo.js
@@ -1,58 +1,66 @@
-// Insert styles
-var CSS = `
-  .shopify-tools-theme-info {
-    font-size: 1.2rem;
-    font-style: italic;
-    color: #81878d;
-  }
-`
-if (!document.querySelector('.shopify-tools-css')) {
-  $('head').append('<style class="shopify-tools-css">');
-}
-
-$('.shopify-tools-css')[0].sheet.insertRule(CSS, 0);
-
 // Regex to use to find theme id in url
 var THEME_ID_REGEX = new RegExp(/\/([0-9]*)\//);
 
-// Get theme information
-$.get(`${document.origin}/admin/themes.json`)
-  .always((response) => {
-    // In this case, we don't have a status if the request was successful
-    if (response.status) {
-      console.error(`There was a problem getting theme info.
-        Reponse came back with ${response.status} - ${response.statusText}`
-      );
-      return;
-    }
-
-    var themeInfo = response.themes;
-    var $themes = $('.published-theme, .unpublished-themes');
-    
-    for (let index = 0; index < $themes.length; index++) {
-      let $currentTheme = $($themes[index]);
-      currentId = null; 
-      if ($currentTheme.hasClass('published-theme')) {
-        let url = ($currentTheme.find('[href*="/editor"]').attr('href'));
-        currentId = THEME_ID_REGEX.exec(url)[1];
-      } else {
-        currentId = $currentTheme.attr('id').replace('theme_', "");
+// Make sure theme info is not already displayed
+if ($('.shopify-tools-theme-info').length === 0) {
+  // Get theme information
+  $.get(`${document.origin}/admin/themes.json`)
+    .always((response) => {
+      // In this case, we don't have a status if the request was successful
+      if (response.status) {
+        console.error(`There was a problem getting theme info.
+          Reponse came back with ${response.status} - ${response.statusText}`
+        );
+        return;
       }
-      let currentInfo = matchThemeInfo(currentId, themeInfo);
-      let $target = $currentTheme.find('[class*="theme-title"]').parent();
-      $target.append(generateThemeInfoHTML(currentInfo));
-    }
-  });
+
+      var themeInfo = response.themes;
+      var $themes = $('.published-theme, .unpublished-themes');
+      
+      for (let index = 0; index < $themes.length; index++) {
+        let $currentTheme = $($themes[index]);
+        let currentId = null; 
+        if ($currentTheme.hasClass('published-theme')) {
+          let url = ($currentTheme.find('[href*="/editor"]').attr('href'));
+          currentId = THEME_ID_REGEX.exec(url)[1];
+        } else {
+          currentId = $currentTheme.attr('id').replace('theme_', "");
+        }
+        let currentInfo = matchThemeInfo(currentId, themeInfo);
+        let $target = $currentTheme.find('[class*="theme-title"]').parent();
+        $target.append(generateThemeInfoHTML(currentInfo));
+
+        // If theme is processsing, watch for when it finishes
+        if (currentInfo.processing) {
+          watchTheme(currentInfo);
+        }
+      }
+    });
+}
 
 // Helper function
 function generateThemeInfoHTML(info) {
   return `
-    <br/>
     <div class="shopify-tools-theme-info">
       <p>Theme ID: ${info.id}</p>
       <p>Last updated ${moment(info.updated_at).fromNow()}</p>
     </div>
   `;
+}
+
+// Watch a processing theme and append theme info when it finishes
+function watchTheme(themeInfo) {
+  const selector = `#theme_${themeInfo.id}`;
+  let $target = $(selector);
+  const mutationTarget = $target.parent()[0];
+
+  const observer = new MutationObserver((mutation) => {
+    observer.disconnect();
+    $target = $(selector).find('[class*="theme-title"]').parent();
+    $target.append(generateThemeInfoHTML(themeInfo));
+  });
+
+  observer.observe(mutationTarget, {childList: true});
 }
 
 function matchThemeInfo(themeId, themeInfo) {
